389a707e9fa3d64fa7c146aa3e4dbee6f91eef7d
BurloiuCosmin <c0sm1@yahoo.com>
2018-07-25 01:18:28 +0300

    Add files via upload

---
+++ notes/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <h1>NOTES</h1>
+</head>
+<body align="center">
+<p><h3>Online Notepad</h3></p>
+
+<?php
+$path = "saved";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$i . $file</a><br><br>";
+        $i++;
+
+    }
+}
+closedir($dh);
+?>
+
+
+<button class="button"><a href="editor.php">Add a new note</a></button>
+<button class="button"><a href="loadnote.php?x=add">Edit a note</a></button>
+<button class="button"><a href="view-functions.php">View a note</a></button>
+<br>
+<br>
+<br>
+<button class="button" onclick="alert('YOU ARE A MADMAN!!!!')"><a href="scratch/deleteall.php">ZELETE THEM ALL</a></button>
+
+
+
+</body>
+</html>
+
